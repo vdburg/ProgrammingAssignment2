79a22d4487ca26ab1eb90bbb1c531c6bb5b169c5
vdburg <vanderburg.robert@gmail.com>
2016-02-21 13:27:05 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The Assignment main objective is to calculate the inverse of a Matrix,
+# but if the value of the inverse is already calculated it can be looked up in the cache
+# rather than recomputed to avoid potentially time-consuming computations
+# The assignment is to write a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+#criteria:
+#x: input matrix
+#s: new input to be loaded
+#i.s: inverse of s
+#r: loaded inverse of s
 
-makeCacheMatrix <- function(x = matrix()) {
-
+### writing the makeCacheMatrix function
+makeCacheMatrix <- function(x = matrix())
+{
+#save matrix in cache
+s<<-x 
+#calculate inverse and assign it to i.s (Inverse of S)
+i.s<-solve(s)
+#Save in cache
+r<<-i.s
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+### writing the cacheSolve function
+cacheSolve <- function(x, ...) 
+ {####start function
+#test if I already have an identical matrix stored and calculated its inverse
+if(identical(s,x)==TRUE)
+ {#### start if statement
+#If true 
+message("getting cached data")
+# and print already calculated inverse (r)
+print(r)} 
+#if not 
+else {
+#store matrix in cache
+s<<-x 
+#calculate inverse and assign it to i.s (Inverse of S)
+i.s<-solve(s)
+#look up in cache
+r<<-i.s
+#print inverse
+print(r)
+}### end of if statement
+   } ### end function
